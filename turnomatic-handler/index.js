9029f70e82ea99abd65ce18af9b4bd1adef1e98d
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-10-26 11:20:51 +0200

    fix: host binding for fastify

--- turnomatic-handler/index.js
+++ turnomatic-handler/index.js
@@ -8,7 +8,7 @@ fastify.get('/turn/:group', (request, reply) => {
 })
 
 // Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen(3000, '0.0.0.0',  err => {
     if (err) {
       fastify.log.error(err)
       process.exit(1)
