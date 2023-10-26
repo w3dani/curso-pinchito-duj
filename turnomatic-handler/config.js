b8e06eba4cd1e6030a8a7c67ac110ecdd7b6fcdc
Daniel Alarcon <daniel.alarcon@onebeyond.com>
2023-10-26 11:34:20 +0200

    feat: connected to rabbit on handler

---
+++ turnomatic-handler/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "$schema": "./node_modules/rascal/lib/config/schema.json",
+    "vhosts": {
+        "/": {
+            "connection": {
+                "url": process.env.RABBITMQ_HOST,
+            },
+            "exchanges": ["turnomatic"],
+            "queues": ["turnomatic-queue", "turnomatic-response-queue"],
+            "bindings": ["turnomatic[turnomatic-queue-key] -> turnomatic-queue"],
+            "publications": {
+                "turnomatic-queue": {
+                    "exchange": "turnomatic",
+                    "routingKey": "turnomatic-queue-key"
+                }
+            },
+            "subscriptions": {
+                "turnomatic-response": {
+                    "queue": "turnomatic-response",
+                    "prefetch": 3
+                }
+            }
+        }
+    }
+}
